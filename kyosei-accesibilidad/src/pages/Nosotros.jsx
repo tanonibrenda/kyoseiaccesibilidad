d484b6b271f665ad37d0bb923e40c7c98c1bd85f
tanonibrenda <brendayohenatanoni@gmail.com>
2025-06-10 17:49:14 -0300

    misionH1Ok

--- kyosei-accesibilidad/src/pages/Nosotros.jsx
+++ kyosei-accesibilidad/src/pages/Nosotros.jsx
@@ -7,34 +7,26 @@ const Nosotros = () => {
   return (
     <>
       {/* <Header /> */}
-      <main className="container d-flex flex-column flex-lg-row align-items-center justify-content-between gap-4 my-5">
-        {/* Sección de texto */}
+      <main className="contenedor">
+        {/* Título centrado arriba */}
+        <h1 className="titulo">Nosotros</h1>
 
-        <main class="contenedor">
-          <h1 className="titulo">Nosotros</h1>
-  
-  <aside class="imagen">
-    <img src="../../public/assets/MISIÓN.png" alt="Manos protegiendo figuras de papel representando diversidad e inclusión como nuestra misión"/>
-  </aside>
+        <div className="contenido">
+          {/* Imagen a la izquierda */}
+          <aside className="imagen">
+            <img src="/assets/MISIÓN.png" alt="Manos protegiendo figuras de papel representando diversidad e inclusión como nuestra misión" />
+          </aside>
 
-  
-  {/* <section class="className=text-lg-start text-center flex-fill">
-    <h2>Nuestra Misión</h2>
-    <p className="lead text-dark">Construimos un mundo más accesible y justo a través de servicios especializados en accesibilidad. 
-    Comenzando por transformar la educación virtual mediante la capacitación docente y el testing de accesibilidad, 
-    trabajamos para que cada persona, sin importar sus capacidades, pueda participar plenamente en la era digital.</p>
-  </section> */}
-</main>
-
-        <section className="text-lg-start text-center flex-fill">
-          <h2 className="display-4 fw-bold text-primary">Nuestra Misión</h2>
-          <p className="lead text-dark">
-            Construimos un mundo más accesible y justo a través de servicios especializados en accesibilidad. 
-    Comenzando por transformar la educación virtual mediante la capacitación docente y el testing de accesibilidad, trabajamos para que cada persona, sin importar sus capacidades, pueda participar plenamente en la era digital.
-          </p>
-        </section>
-
-       
+          {/* Texto a la derecha */}
+          <section className="texto">
+            <h2>Nuestra Misión</h2>
+            <p className="parrafo">
+              Construimos un mundo más accesible y justo a través de servicios especializados en accesibilidad.
+              Comenzando por transformar la educación virtual mediante la capacitación docente y el testing de accesibilidad, 
+              trabajamos para que cada persona, sin importar sus capacidades, pueda participar plenamente en la era digital.
+            </p>
+          </section>
+        </div>
       </main>
       {/* <Footer /> */}
     </>
